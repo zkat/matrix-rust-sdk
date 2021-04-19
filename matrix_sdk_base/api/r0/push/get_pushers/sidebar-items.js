c59a34482d36e0d0e2c6002c9cd65cda5f024a1e
poljar <poljar@users.noreply.github.com>
2021-04-19 13:09:37 +0000

    deploy: 65d84c111ba416d85b097055996deb424ffdfd67

--- matrix_sdk_base/api/r0/push/get_pushers/sidebar-items.js
+++ matrix_sdk_base/api/r0/push/get_pushers/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["METADATA","Metadata for the `get_pushers` API endpoint."]],"struct":[["Request","Data for a request to the `get_pushers` API endpoint."],["Response","Data in the response from the `get_pushers` API endpoint."]]});
+initSidebarItems({"constant":[["METADATA","Metadata for the `get_pushers` API endpoint."]],"struct":[["Pusher","Defines a pusher."],["Request","Data for a request to the `get_pushers` API endpoint."],["Response","Data in the response from the `get_pushers` API endpoint."]]});
