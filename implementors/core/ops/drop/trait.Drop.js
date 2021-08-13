3f8709df82af0614aa38f784002cbea0c2aee941
poljar <poljar@users.noreply.github.com>
2021-08-13 18:48:57 +0000

    deploy: 623408913c6d557c37301e560cd7f1dcf4d79841

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["matrix_sdk_crypto"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"matrix_sdk_crypto/store/struct.CrossSigningKeyExport.html\" title=\"struct matrix_sdk_crypto::store::CrossSigningKeyExport\">CrossSigningKeyExport</a>","synthetic":false,"types":["matrix_sdk_crypto::store::CrossSigningKeyExport"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
