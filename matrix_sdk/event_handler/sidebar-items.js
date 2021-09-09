9dea37261c550ca710da895fbc71038d9c705ff2
poljar <poljar@users.noreply.github.com>
2021-09-09 08:11:52 +0000

    deploy: 0e9a1b42ec1c920ae1fa23cbd11380486b6dd51a

---
+++ matrix_sdk/event_handler/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["RawEvent","The raw JSON form of an event."]],"trait":[["EventHandler","Interface for event handlers."],["EventHandlerContext","Context for an event handler."],["EventHandlerResult","Return types supported for event handlers implement this trait."],["SyncEvent","A statically-known event kind/type that can be retrieved from an event sync."]]});
