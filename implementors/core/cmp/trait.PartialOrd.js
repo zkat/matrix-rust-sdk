4a88471f1199cb8fe8756491ecf16f5262f2965c
poljar <poljar@users.noreply.github.com>
2021-11-08 16:52:06 +0000

    deploy: 05a190ee5ab1cbee4d827415f6cd31dcdb40db0f

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["matrix_sdk_crypto"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"matrix_sdk_crypto/struct.Emoji.html\" title=\"struct matrix_sdk_crypto::Emoji\">Emoji</a>&gt; for <a class=\"struct\" href=\"matrix_sdk_crypto/struct.Emoji.html\" title=\"struct matrix_sdk_crypto::Emoji\">Emoji</a>","synthetic":false,"types":["matrix_sdk_crypto::verification::Emoji"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
