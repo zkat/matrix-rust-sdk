e1238e9fb48529e7322d0c9a97b680366bda0959
jplatte <jplatte@users.noreply.github.com>
2022-03-04 11:15:29 +0000

    deploy: 896d451c47abce80a86263be36c2c772779f521c

---
+++ implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["xtask"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"xtask/ci/enum.FeatureSet.html\" title=\"enum xtask::ci::FeatureSet\">FeatureSet</a>","synthetic":false,"types":["xtask::ci::FeatureSet"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
