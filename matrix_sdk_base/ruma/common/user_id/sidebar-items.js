5b70f302be725842dd463fa42495b13f7805b8db
jplatte <jplatte@users.noreply.github.com>
2022-04-12 12:41:43 +0000

    deploy: 35f598a09582d69a7c73de806c63318ca10b7d39

--- matrix_sdk_base/ruma/common/user_id/sidebar-items.js
+++ matrix_sdk_base/ruma/common/user_id/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["localpart_is_fully_conforming","Check whether the given user id localpart is valid and fully conforming"]],"struct":[["UserId","A Matrix user ID."]]});
+initSidebarItems({"fn":[["localpart_is_fully_conforming","Check whether the given user id localpart is valid and fully conforming"]],"struct":[["OwnedUserId","Owned variant of UserId"],["UserId","A Matrix user ID."]]});
