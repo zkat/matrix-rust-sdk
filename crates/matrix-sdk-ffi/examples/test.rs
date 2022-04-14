7dca9599d16db196f258229a57a3cbb226fce282
Jonas Platte <jplatte@element.io>
2022-04-14 13:42:16 +0200

    Update ffi backward stream test example to pull out multiple batches of events

--- crates/matrix-sdk-ffi/examples/test.rs
+++ crates/matrix-sdk-ffi/examples/test.rs
@@ -1,19 +1,15 @@
 use std::sync::Arc;
 
 use futures_util::StreamExt;
-use matrix_sdk::{config::SyncSettings, ruma::RoomId, LoopCtrl, deserialized_responses::SyncRoomEvent, Error};
-use matrix_sdk_ffi::{*, client::Client};
+use matrix_sdk::{
+    config::SyncSettings, deserialized_responses::SyncRoomEvent, ruma::RoomId, Error, LoopCtrl,
+};
+use matrix_sdk_ffi::{client::Client, *};
 
 fn main() {
-
     let room_id = <&RoomId>::try_from("!NMoyFpQxIVYaQIfQHG:matrix.org").unwrap();
 
-    let client = login_new_client(
-        "./RustTest".to_owned(),
-        "".to_owned(),
-        "".to_owned(),
-    )
-    .unwrap();
+    let client = login_new_client("./RustTest".to_owned(), "".to_owned(), "".to_owned()).unwrap();
 
     let restore_token = client.restore_token().unwrap();
     fetch_backward_batch(client, room_id);
@@ -26,9 +22,13 @@ fn fetch_backward_batch(client: Arc<Client>, room_id: &RoomId) {
     RUNTIME.block_on(client.sync_with_callback(SyncSettings::default(), |_| async {
         let room = client.get_room(room_id).unwrap();
 
-        let backward_stream = room.timeline_backward().await.expect("Failed acquiring timeline streams");
+        let mut backward_stream =
+            Box::pin(room.timeline_backward().await.expect("Failed acquiring timeline streams"));
 
-        let results = backward_stream.take(30).collect::<Vec<_>>().await;
+        let results = backward_stream.by_ref().take(30).collect::<Vec<_>>().await;
+        print_results(results);
+
+        let results = backward_stream.by_ref().take(30).collect::<Vec<_>>().await;
         print_results(results);
 
         LoopCtrl::Break
@@ -36,17 +36,14 @@ fn fetch_backward_batch(client: Arc<Client>, room_id: &RoomId) {
 }
 
 fn print_results(results: Vec<Result<SyncRoomEvent, Error>>) {
-    for result in results.iter() {
-        match result {
-            Ok(event) => { 
-                match event.event_id() {
-                    Some(id) => { println!("{}", id)}
-                    _ => {}
-                }
-             }
-            _ => {}
+    #[allow(clippy::manual_flatten)]
+    for result in &results {
+        if let Ok(event) = result {
+            if let Some(id) = event.event_id() {
+                println!("{}", id);
+            }
         }
-    }    
+    }
 
-    println!("")
+    println!();
 }
