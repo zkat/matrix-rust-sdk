6f352d6cdf2bb7a99898189f298a7de14bce0b2a
gnunicorn <gnunicorn@users.noreply.github.com>
2022-04-19 14:50:37 +0000

    deploy: 06f5b67b238eda032db26f182c15db6eedc56f6b

--- implementors/core/convert/trait.Into.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["matrix_sdk_base"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"enum\" href=\"matrix_sdk_base/store/enum.StoreError.html\" title=\"enum matrix_sdk_base::store::StoreError\">StoreError</a>&gt; for <a class=\"enum\" href=\"matrix_sdk_base/store/store_key/enum.Error.html\" title=\"enum matrix_sdk_base::store::store_key::Error\">Error</a>","synthetic":false,"types":["matrix_sdk_base::store::store_key::Error"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- matrix_sdk_store_encryption/sidebar-items.js
+++ matrix_sdk_store_encryption/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error","Error type for the `StoreCipher` operations."]],"struct":[["StoreCipher","An encryption key that can be used to encrypt data for key/value stores."]]});
+initSidebarItems({"enum":[["Error","Error type for the `StoreCipher` operations."]],"struct":[["EncryptedValue","Encrypted value, ready for storage, as created by the [`StoreCipher::encrypt_value_data()`]"],["StoreCipher","An encryption key that can be used to encrypt data for key/value stores."]]});
