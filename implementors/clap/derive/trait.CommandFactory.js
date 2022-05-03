7797a2d4cfc22a129c2cbbd42be6512a99b7615d
gnunicorn <gnunicorn@users.noreply.github.com>
2022-05-03 08:24:52 +0000

    deploy: 7dd7f5a6116f9c3286c276cbfb69f0dce8e8a574

--- implementors/clap/derive/trait.CommandFactory.js
+++ implementors/clap/derive/trait.CommandFactory.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["xtask"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/clap/3.1.14/clap/derive/trait.CommandFactory.html\" title=\"trait clap::derive::CommandFactory\">CommandFactory</a> for <a class=\"struct\" href=\"xtask/struct.Xtask.html\" title=\"struct xtask::Xtask\">Xtask</a>","synthetic":false,"types":["xtask::Xtask"]}];
+implementors["xtask"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/clap/3.1.15/clap/derive/trait.CommandFactory.html\" title=\"trait clap::derive::CommandFactory\">CommandFactory</a> for <a class=\"struct\" href=\"xtask/struct.Xtask.html\" title=\"struct xtask::Xtask\">Xtask</a>","synthetic":false,"types":["xtask::Xtask"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
