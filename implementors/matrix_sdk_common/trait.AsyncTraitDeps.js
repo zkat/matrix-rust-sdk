a0e1ced70d732f739591b3280971504bc34be242
jplatte <jplatte@users.noreply.github.com>
2022-05-09 10:42:46 +0000

    deploy: 0fad7b60a9497b472beac6f4bc55bc091dd7ef65

--- implementors/matrix_sdk_common/trait.AsyncTraitDeps.js
+++ implementors/matrix_sdk_common/trait.AsyncTraitDeps.js
@@ -1,3 +1,5 @@
 (function() {var implementors = {};
+implementors["matrix_sdk"] = [];
+implementors["matrix_sdk_base"] = [];
 implementors["matrix_sdk_common"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
